86c0d9f764e0260efc456c555a24cb86397ff75e
badcel <1218031+badcel@users.noreply.github.com>
2022-10-08 21:57:25 +0200

    Fix string length detection - Unicode string length detection only checked one byte instead of both - UTF32 string length detection only checked one byte instead of all four - Added Unit tests to test for special chars which would provoke wrong string length detection

---
+++ src/HidApi.Net.Tests/StringTests.cs
@@ -0,0 +1,56 @@
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HidApi.Tests;
+
+[TestClass]
+public class StringTests
+{
+    [TestMethod]
+    public void TestUnicode()
+    {
+        var data = new byte[6];
+        data[0] = 0;
+        data[1] = 1;
+        data[2] = 0;
+        data[3] = 1;
+        data[4] = 0;
+        data[5] = 0;
+
+        unsafe
+        {
+            fixed (byte* p = data)
+            {
+                var str = Unicode.Read(p);
+                str.Should().Be(new string(new[] { '\u0100', '\u0100' }));
+            }
+        }
+    }
+
+    [TestMethod]
+    public void TestUtf32()
+    {
+        var data = new byte[12];
+        data[0] = 0;
+        data[1] = 1;
+        data[2] = 0;
+        data[3] = 0;
+        data[4] = 0;
+        data[5] = 1;
+        data[6] = 0;
+        data[7] = 0;
+        data[8] = 0;
+        data[9] = 0;
+        data[10] = 0;
+        data[11] = 0;
+
+        unsafe
+        {
+            fixed (byte* p = data)
+            {
+                var str = Utf32.Read(p);
+                str.Should().Be(new string(new[] { '\u0100', '\u0100' }));
+            }
+        }
+    }
+}
