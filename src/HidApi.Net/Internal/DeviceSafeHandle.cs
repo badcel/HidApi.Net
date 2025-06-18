1c5427d485c4614d245dcedb01c0e25a91f3b136
badcel <1218031+badcel@users.noreply.github.com>
2025-06-18 17:13:58 +0200

    Update supported dotnet runtimes - Drop dotnet 6 and dotnet 7 support - Add dotnet 9 support

--- src/HidApi.Net/Internal/DeviceSafeHandle.cs
+++ src/HidApi.Net/Internal/DeviceSafeHandle.cs
@@ -2,12 +2,10 @@ using System.Runtime.InteropServices;
 
 namespace HidApi;
 
-internal class DeviceSafeHandle : SafeHandle
+internal class DeviceSafeHandle() : SafeHandle(IntPtr.Zero, true)
 {
     public static readonly DeviceSafeHandle Null = new();
     public override bool IsInvalid => handle == IntPtr.Zero;
-
-    public DeviceSafeHandle() : base(IntPtr.Zero, true) { }
 
     protected override bool ReleaseHandle()
     {
